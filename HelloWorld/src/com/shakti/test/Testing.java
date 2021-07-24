c35a51ac2fa3b84b29edc6ab6f06ace3d1fa33f6
gittl <giit@gmail.com>
2021-07-24 23:31:22 +0530

    commit by Team Leader adding Testing.java to test package

---
+++ HelloWorld/src/com/shakti/test/Testing.java
@@ -0,0 +1,14 @@
+package com.shakti.test;
+
+import com.shakti.beans.GitTest;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+		GitTest gt=new GitTest();
+		System.out.println(gt.getSystemDate());
+
+	}
+
+}
